7a74d96d02350f8d9518d548dd660beccc12826d
Dave Pratt <davepratt@roosterteeth.com>
2023-05-16 20:03:47 -0400

    Resetting to better default

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from components.vectorstore_conversation import VectorstoreConversation
 
 def main():
 	human_name = input(" Enter your name, please: ")
-	companion = Mel(id='Mel', mute=False, human_name=human_name)
+	companion = Mel(id='Mel', mute=True, human_name=human_name)
 	# if this is your first time running, set both first_time and clear_store to True - this will get the vectorstore set up right
 	# once vectorstore is set up, change these both to False to keep memory between sessions. 
 	session = VectorstoreConversation(companion=companion, first_time=False, clear_store=False)
